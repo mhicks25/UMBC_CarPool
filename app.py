8191341177d301873aaefd283f74200366bc08aa
Marieana H <mhicks6@umbc.edu>
2024-02-29 10:21:37 -0500

    modified:   app.py 	new file:   templates/index.html

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
 
+app = Flask(__name__)
+
+#index
+@app.route('/')
+def index():
+    return render_template('index.html')
